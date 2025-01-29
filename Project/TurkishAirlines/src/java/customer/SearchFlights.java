46edba7b4728fe6f0285f218595851d4a7e2872c
Diego Cruz Camargo <diego.cruz.camargo@gmail.com>
2025-01-29 12:10:49 -0300

    Adicionando correção do Sonar

--- Project/TurkishAirlines/src/java/customer/SearchFlights.java
+++ Project/TurkishAirlines/src/java/customer/SearchFlights.java
@@ -37,38 +37,7 @@ public class SearchFlights extends HttpServlet {
 
         ArrayList<Flight> flights = (ArrayList<Flight>) (getServletContext().getAttribute("flights"));
 
-        DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-
-        ArrayList<Flight> results = new ArrayList();
-        
-        /*
-        for (int i = 0; i < flights.size(); i++)
-        {            
-            Flight f = flights.get(i);
-                        
-            if ((f.getDepartureCity().equals(request.getParameter("from"))) && (f.getArrivalCity().equals(request.getParameter("to")))  && (reportDate.equals(request.getParameter("depart"))))
-            {                                
-                if (f.getCurrentSeats() >= Integer.parseInt(request.getParameter("people")))
-                {
-                    if (request.getParameter("class").equals("Economy") && (f.getEconomySeats()>= Integer.parseInt(request.getParameter("people"))))
-                    {
-                        if (!f.isChanged && !Features.isChanged)                        
-                            results.add(f);                                       
-                    }
-                    else if (request.getParameter("class").equals("Business") && (f.getBusinessSeats()>= Integer.parseInt(request.getParameter("people"))))
-                    {
-                        if (!f.isChanged && !Features.isChanged)                        
-                            results.add(f);                                       
-                    }
-                    else if (request.getParameter("class").equals("First") && (f.getFirstSeats()>= Integer.parseInt(request.getParameter("people"))))
-                    {
-                        if (!f.isChanged && !Features.isChanged)                        
-                            results.add(f);                                       
-                    }
-                }
-            }            
-        }
-        */
+        ArrayList<Flight> results = new ArrayList<>();        
         
         results.add(flights.get(1));
         request.setAttribute("results", results);
